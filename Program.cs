be73d685f4a62f21d75d274839776624fa0a1836
kes-laksirif <laksiri.fernando@kingslake.com>
2020-02-07 10:35:47 +0530

    Initial git commit of project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var msg = "Hello Open Source World!";
+            Console.WriteLine(msg);
+        }
+    }
+}
